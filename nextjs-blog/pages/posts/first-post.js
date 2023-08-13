0525335e32ced0f88bcf7df41ceed14eba991f61
Rafael Lima <rafauxdev@gmail.com>
2023-08-13 16:51:33 +0100

    Commit random

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,34 @@
 import Link from "next/link"
+import Image from "next/image"
 import React from "react"
+import Head from "next/head"
+import Script from "next/script"
+import Layout from "../../components/Layout/Layout"
 
 function FirstPost() {
   return (
-    <div>
-      <h1>FirstPost</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
+      <div>
+        <h1>FirstPost</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+        <div>
+          <Image src={"/images/dog.png"} width={144} height={144} />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
